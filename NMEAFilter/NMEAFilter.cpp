fb15b2d83b18b26c78a3c0a21fd54097edb42e4b
Stephen Quan <squan@esri.com>
2019-04-11 15:18:32 +1000

    Replace TABs with spaces

--- NMEAFilter/NMEAFilter.cpp
+++ NMEAFilter/NMEAFilter.cpp
@@ -42,55 +42,55 @@ extern "C" APEXDECL LRESULT APExTerminate(void)
 
 extern "C" APEXDECL LRESULT APExPreTranslateNMEA(LPSTR pSentence, int nFields, LPCSTR pFields[], INSTRUMENT *pInstrument)
 {
-	char Filters[1024] = "!$BD;$";
-	char* FiltersEnv = getenv("NMEAFILTERS");
-	char* DebugEnv = getenv("NMEAFILTERS_DEBUG");
-	int debugMode = (DebugEnv != NULL && strcmp(DebugEnv, "YES") == 0) ? 1 : 0;
+    char Filters[1024] = "!$BD;$";
+    char* FiltersEnv = getenv("NMEAFILTERS");
+    char* DebugEnv = getenv("NMEAFILTERS_DEBUG");
+    int debugMode = (DebugEnv != NULL && strcmp(DebugEnv, "YES") == 0) ? 1 : 0;
 
-	static DWORD lastShownAccept = 0;
-	static DWORD lastShownReject = 0;
+    static DWORD lastShownAccept = 0;
+    static DWORD lastShownReject = 0;
 
-	char* pFilter = (FiltersEnv != NULL && *FiltersEnv != '\0') ? FiltersEnv : Filters;
-	while (pFilter && *pFilter != '0')
-	{
-		char* pFilterEnd = strchr(pFilter, ';');
-		if (*pFilter == '!')
-		{
-			pFilter++;
-			int n = pFilterEnd ? (pFilterEnd - pFilter) : strlen(pFilter);
-			if (strncmp(pSentence, pFilter, n) == 0)
-			{
-				if (debugMode > 0)
-				{
-					if (GetTickCount() > lastShownReject + 10000)
-					{
-						MessageBoxA(g_hWnd, pSentence, "Reject", MB_OK);
-						lastShownReject = GetTickCount();
-					}
-				}
-				return APEX_TRUE;
-			}
-		}
-		else
-		{
-			int n = pFilterEnd ? (pFilterEnd - pFilter) : strlen(pFilter);
-			if (strncmp(pSentence, pFilter, n) == 0)
-			{
-				if (debugMode > 0)
-				{
-					if (GetTickCount() > lastShownAccept + 10000)
-					{
-						MessageBoxA(g_hWnd, pSentence, pFilter, MB_OK);
-						lastShownAccept = GetTickCount();
-					}
-				}
-				return APEX_FALSE;
-			}
-		}
-		pFilter = pFilterEnd ? pFilterEnd + 1 : NULL;
-	}
+    char* pFilter = (FiltersEnv != NULL && *FiltersEnv != '\0') ? FiltersEnv : Filters;
+    while (pFilter && *pFilter != '0')
+    {
+        char* pFilterEnd = strchr(pFilter, ';');
+        if (*pFilter == '!')
+        {
+            pFilter++;
+            int n = pFilterEnd ? (pFilterEnd - pFilter) : strlen(pFilter);
+            if (strncmp(pSentence, pFilter, n) == 0)
+            {
+                if (debugMode > 0)
+                {
+                    if (GetTickCount() > lastShownReject + 10000)
+                    {
+                        MessageBoxA(g_hWnd, pSentence, "Reject", MB_OK);
+                        lastShownReject = GetTickCount();
+                    }
+                }
+                return APEX_TRUE;
+            }
+        }
+        else
+        {
+            int n = pFilterEnd ? (pFilterEnd - pFilter) : strlen(pFilter);
+            if (strncmp(pSentence, pFilter, n) == 0)
+            {
+                if (debugMode > 0)
+                {
+                    if (GetTickCount() > lastShownAccept + 10000)
+                    {
+                        MessageBoxA(g_hWnd, pSentence, pFilter, MB_OK);
+                        lastShownAccept = GetTickCount();
+                    }
+                }
+                return APEX_FALSE;
+            }
+        }
+        pFilter = pFilterEnd ? pFilterEnd + 1 : NULL;
+    }
 
-	return APEX_FALSE;
+    return APEX_FALSE;
 }
 
 //----------------------------------------------------------------------
